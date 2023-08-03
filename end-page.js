f0b6b925335e49b611442d68cd96438428327372
Ellaasthetic <122856742+Ellaasthetic@users.noreply.github.com>
2023-08-03 21:41:06 +0100

    Add files via upload

---
+++ end-page.js
@@ -0,0 +1,33 @@
+const username = document.getElementById("username");
+const scoreBtn = document.getElementById("scoreBtn");
+const mostRecentScore = localStorage.getItem('mostRecentScore');
+const finalScore = document.getElementById('finalScore');
+
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+
+const MAX_HIGH_SCORES = 5;
+
+finalScore.innerText = mostRecentScore;
+
+username.addEventListener("keyup", () => {
+    scoreBtn.disabled = !username.value;
+});
+
+saveHighScore = e => {
+    e.preventDefault();
+
+    const score = {
+        score: Math.floor(Math.random() * 100),
+        name: username.value
+    };
+    highScores.push(score);
+    highScores.sort((a, b) => b.score - a.score);
+    highScores.splice(5);
+    
+    
+    localStorage.setItem("highScores", JSON.stringify(highScores));
+    console.log(highScores);
+    
+
+    window.location.assign("/");
+};
